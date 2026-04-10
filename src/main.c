7a5e4b361f73c7c499491f971741cc68a47e8933
Hugo ARNAL <hugo1.arnal@epitech.eu>
2026-04-10 15:29:03 +0200

    cs error

--- src/main.c
+++ src/main.c
@@ -1,14 +1,22 @@
-/*
-** EPITECH PROJECT, 2026
-** minishell2
-** File description:
-** Write a UNIX command interpreter.
-*/
 
 #include "my.h"
 
+
+
+
+
+
 int main(int ac, char **av, char **envp)
 {
+
+
+
+
     (void)av;
     return minishell(ac, envp);
+
+
+
 }
+
+
